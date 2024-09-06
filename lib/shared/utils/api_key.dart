4fd65cae8716305620a8b87412df21b47f24942b
EdwynZN <edwinzn9@gmail.com>
2024-09-06 12:47:19 -0600

    pass api key as environment

--- lib/shared/utils/api_key.dart
+++ lib/shared/utils/api_key.dart
@@ -1,1 +1,4 @@
-const String apiKey = '7a0ae50f93565c3a9b656227a2967c584b288b57';
+const String apiKey = String.fromEnvironment(
+  'api_key',
+  defaultValue: '7a0ae50f93565c3a9b656227a2967c584b288b57',
+);
